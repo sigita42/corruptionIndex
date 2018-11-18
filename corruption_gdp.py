658b85ebd1154bf7f0a7233cc0aeee98a1f0ef56
Sigita <sigita.grinfelde@gmail.com>
2018-11-18 22:01:07 +0100

    Plot PCI change in 4 countries

--- corruption_gdp.py
+++ corruption_gdp.py
@@ -22,11 +22,29 @@ pprint(data)
 header = data["elements"][1]["data"][0][0]
 datavalues = data["elements"][1]["data"][0][1:]
 
-df = pd.DataFrame(data = dataset, columns = header)
+df = pd.DataFrame(data = datavalues, columns = header)
 
-#Test
+#Test plot
 X = df.iloc[:, 1:8].values
-denmark = X[1,1:]
-years = ["2017", "2016", "2015", "2014", "2013", "2012"]
-var = []
 
+years = [2012, 2013, 2014, 2015, 2016, 2017]
+
+denmark = list(map(int, X[1,1:]))
+denmark.reverse()
+
+estonia = list(map(int, X[20,1:]))
+estonia.reverse()
+
+latvia = list(map(int, X[39,1:]))
+latvia.reverse()
+
+lithuania = list(map(int, X[38,1:]))
+lithuania.reverse()
+
+ 
+# Plot the change in PCI in 4 countries
+plt.plot( years, denmark, marker='o', markerfacecolor='olive', markersize=8, color='skyblue', linewidth=3, label="Denmark")
+plt.plot( years, latvia, marker='', color='red', linewidth=2, linestyle='dashed', label="Latvia")
+plt.plot( years, lithuania, marker='', color='yellow', linewidth=2, linestyle='dashed', label="Lithuania")
+plt.plot( years, estonia, marker='', color='blue', linewidth=2, linestyle='dashed', label="Estonia")
+plt.legend()
