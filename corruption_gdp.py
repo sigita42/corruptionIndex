6f3f6e901ff557956bd889f6c1c6b6947ac4b8dc
sigita42 <sigita42@users.noreply.github.com>
2018-11-18 17:07:51 +0100

    Add python script, Add dataset
    Add python script that reads dataset in json format

---
+++ corruption_gdp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 18 11:19:33 2018
+
+@author: sigita
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
+from pprint import pprint
+
+
+path = "/home/sigita/Dropbox/LearnProgramming/transparency_international/dataset.json"
+
+with open(path) as f:
+    data = json.load(f)
+
+pprint(data)
+
+header = data["elements"][1]["data"][0][0]
+datavalues = data["elements"][1]["data"][0][1:]
+
+df = pd.DataFrame(data = dataset, columns = header)
+
+#Test
+X = df.iloc[:, 1:8].values
+denmark = X[1,1:]
+years = ["2017", "2016", "2015", "2014", "2013", "2012"]
+
